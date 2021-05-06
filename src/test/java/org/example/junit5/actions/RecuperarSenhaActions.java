08e8a64398008e878915773a43375ba4c6aba1ad
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2021-05-06 11:35:39 -0300

    preenche email e clica botao recuperar

---
+++ src/test/java/org/example/junit5/actions/RecuperarSenhaActions.java
@@ -0,0 +1,18 @@
+package org.example.junit5.actions;
+
+import org.example.junit5.pageobjects.RecuperarContaObjects;
+import org.openqa.selenium.WebDriver;
+
+public class RecuperarSenhaActions {
+
+    private RecuperarContaObjects recuperarContaObjects;
+
+    public RecuperarSenhaActions(WebDriver driver) {
+        recuperarContaObjects = new RecuperarContaObjects(driver);
+    }
+
+    public void recuperarSenha(){
+        recuperarContaObjects.getInputRecuperarEmail().sendKeys("email@email.com");
+        recuperarContaObjects.getButRecuperarEmail().click();
+    }
+}
